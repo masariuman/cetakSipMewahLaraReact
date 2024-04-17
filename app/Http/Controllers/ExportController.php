e0fdd5363fcfbb0483598b6e40d03e9b4aec67e4
masariuman <masariuman@gmail.com>
2024-04-17 12:57:06 +0700

    nama file master data + today

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Helper;
 use Illuminate\Http\Request;
 use App\Exports\MasterDataExport;
 use Maatwebsite\Excel\Facades\Excel;
@@ -9,8 +10,16 @@ use App\Models\Pegawai;
 
 class ExportController extends Controller
 {
+    private $helper;
+
+    public function __construct()
+    {
+        $this->helper = new Helper;
+    }
+
     public function masterData() {
-        $filename = "MasterData.xlsx";
+        $today = $this->helper->today();
+        $filename = "MasterData ".$today.".xlsx";
         return Excel::download(new MasterDataExport, $filename);
     }
 }
